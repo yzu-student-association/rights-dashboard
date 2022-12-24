8f279e3c83d4e9529abf1b9370c5fc0a8684028e
fanlan1210 <fanlan1210@users.noreply.github.com>
2022-12-24 18:38:55 +0800

    first commit

---
+++ Card.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container p-px mx-auto h-full">
+        <div :class="statusObj" class="border-t-8 w-4/5 bg-white mx-auto mt-4 border-gray-100 shadow p-5 rounded-md">
+            <div class="flex justify-center">
+                <img class="w-2/3" :src="imgStatus" alt="">
+            </div>
+            <h2 class="font-medium text-2xl">{{name}}</h2>
+            <div v-if="description">
+                <p>{{description}}</p>
+            </div>
+            <div v-else>
+                <p>{{status}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+const statusColor = {
+    "無法辦理": "border-t-red-500",
+    "辦理完成": "border-t-green-500",
+    "提案排程中": "border-t-gray-500",
+    "辦理中": "border-t-blue-500",
+    "研議中": "border-t-yellow-500",
+    "提案中": "border-t-amber-500",
+}
+
+export default {
+    name: 'card',
+    props: ['name','status','description'],
+    data: () => {
+        return {
+
+        }
+    },
+    computed: {
+        statusObj() {
+            return {
+                [statusColor[this.status]]: true,
+            }
+        },
+        imgStatus() {
+            return `/imgs/${this.status}.png`
+        }
+    }
+}
+</script>
---
+++ RingChart.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="main"></div>
+</template>
+
+<script>
+
+export default {
+    name: 'ring-chart',
+    mounted() {
+        var myChart = echarts.init(document.getElementById('main'));
+        
+        let option = {
+  title: {
+    text: '',
+    left: 'center',
+    top: 'center'
+  },
+  series: [
+    {
+      type: 'pie',
+      color: ['lightgray','yellow','skyblue'],
+      data: [
+        {
+          value: 3,
+          name: '提案排程中'
+        },
+        {
+          value: 1,
+          name: '研議中'
+        },
+        {
+          value: 1,
+          name: '辦理中'
+        }
+      ],
+      radius: ['40%', '70%'],
+      label: {formatter: '{b}: {c}'}
+    }
+  ]
+};
+
+        myChart.setOption(option);
+    }
+}
+
+</script>
