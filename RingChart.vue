63caf7400a3091e672b1ed62a62d014379629d2e
fanlan1210 <fanlan1210@users.noreply.github.com>
2023-02-10 13:38:10 +0800

    replace google chart by using ECharts

--- RingChart.vue
+++ RingChart.vue
@@ -3,43 +3,56 @@
 </template>
 
 <script>
+import { isProxy, toRaw } from 'vue';
 
 export default {
     name: 'ring-chart',
-    mounted() {
+    props: {
+      'data':{
+        type: Array
+      }
+    },
+	watch:{
+		data: {
+			handler(val, oldVal){
+				this.updateChart();
+			},
+			deep: true
+		}
+	},
+    methods: {
+      updateChart(){
         var myChart = echarts.init(document.getElementById('main'));
-        
+        let rawData = toRaw(this.data);
+		console.log(rawData);
         let option = {
-  title: {
-    text: '',
-    left: 'center',
-    top: 'center'
-  },
-  series: [
-    {
-      type: 'pie',
-      color: ['lightgray','yellow','skyblue'],
-      data: [
-        {
-          value: 3,
-          name: '提案排程中'
-        },
-        {
-          value: 1,
-          name: '研議中'
-        },
-        {
-          value: 1,
-          name: '辦理中'
-        }
-      ],
-      radius: ['40%', '70%'],
-      label: {formatter: '{b}: {c}'}
-    }
-  ]
-};
-
+          title: {
+            text: '專案進度追蹤',
+            left: 'center',
+            top: 'center',
+			textStyle: {fontSize: 24}
+          },
+          series: [
+            {
+              type: 'pie',
+              color: ['#B5B5B6','#FFF362','#E1893F','#6DA1C8','#73B284','#DD776A'],
+              data: rawData,
+              radius: ['60%', '70%'],
+              label: {formatter: '{b}: {c}',fontSize: 16},
+			  colorBy: 'data',
+            }
+          ],
+		  legend: {
+			show: true,
+			top: 'bottom'
+		  }
+        };
         myChart.setOption(option);
+		
+      }
+    },
+    mounted() {
+        this.updateChart();
     }
 }
 
